56e5a8129af42e59394f9d19a0c773c9cafe4944
Tony Stark <tony_stark2512@proton.me>
2025-07-18 23:36:29 +0700

    fix admin use client

--- admin/app/(protected)/page.tsx
+++ admin/app/(protected)/page.tsx
@@ -5,21 +5,21 @@ import Image from 'next/image'
 export default function HomePage() {
   return (
     <div className="flex flex-col items-center justify-center min-h-[calc(100vh-4rem)] gap-8">
-      <div className="relative w-[200px] h-[80px]">
+      <div className="relative">
         <Image
           src="/clixxx-logo.svg"
           alt="Clixxx Logo"
-          fill
-          style={{ objectFit: 'contain' }}
+          width={200}
+          height={80}
           priority
         />
       </div>
-      <div className="relative w-[300px] h-[600px]">
+      <div className="relative">
         <Image
           src="/fashion-illustration.jpg"
           alt="Fashion Illustration"
-          fill
-          style={{ objectFit: 'contain' }}
+          width={300}
+          height={600}
           priority
         />
       </div>
