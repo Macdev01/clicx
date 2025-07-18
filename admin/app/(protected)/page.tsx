623c27f4c2893d44a3d18deebceefd504b2f7227
Tony Stark <tony_stark2512@proton.me>
2025-07-18 23:26:53 +0700

    add logo

--- admin/app/(protected)/page.tsx
+++ admin/app/(protected)/page.tsx
@@ -4,7 +4,16 @@ import Image from 'next/image'
 
 export default function HomePage() {
   return (
-    <div className="flex items-center justify-center min-h-[calc(100vh-4rem)]">
+    <div className="flex flex-col items-center justify-center min-h-[calc(100vh-4rem)] gap-8">
+      <div className="relative w-[200px] h-[80px]">
+        <Image
+          src="/clixxx-logo.svg"
+          alt="Clixxx Logo"
+          fill
+          style={{ objectFit: 'contain' }}
+          priority
+        />
+      </div>
       <div className="relative w-[300px] h-[600px]">
         <Image
           src="/fashion-illustration.jpg"
